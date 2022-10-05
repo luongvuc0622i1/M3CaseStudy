7ef28b7ed457f1a76d8fc77efcb7def5a458b0f3
wjnzero <chuckieakenzzz@gmai.com>
2022-10-05 20:22:19 +0700

    add model Tag

---
+++ src/main/java/model/Tag.java
@@ -0,0 +1,75 @@
+package model;
+
+public class Tag {
+    private int id, status;
+    private String code, name, description;
+
+    public Tag() {
+    }
+
+    public Tag(int status, String code, String name, String description) {
+        this.status = status;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Tag(int id, int status, String code, String name, String description) {
+        this.id = id;
+        this.status = status;
+        this.code = code;
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", status=" + status +
+                ", code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
