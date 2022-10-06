de085fadebb52c5a66c8a30eb325ca3d79233882
Compass <ndphong165@gmail.com>
2022-10-06 14:25:35 +0700

    fix loi tag

--- src/main/java/service/food/FoodService.java
+++ src/main/java/service/food/FoodService.java
@@ -1,7 +1,15 @@
 package service.food;
 
+import connection.ConnectionCMS;
+
+import java.sql.Connection;
+
 public class FoodService {
 
     private static final String SELECT_FOOD_BY_ID_SHOP = "SELECT * FROM food WHERE shop_id=?";
+    private static final String SELECT_FOOD_BY_ID_DEAL = "SELECT * FROM food WHERE deal_id=?";
+    private static final String SELECT_FOOD_BY_ID_TAGS = "SELECT * FROM food WHERE tags_id=?";
+    Connection connection = ConnectionCMS.getConnection();
+
 
 }
