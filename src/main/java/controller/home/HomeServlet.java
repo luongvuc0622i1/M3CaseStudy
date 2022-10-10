a13d76989b48bbe1c83ee7cf0a3e5d2dc94e096c
TuLiP-411 <65971089+TuLiP-411@users.noreply.github.com>
2022-10-10 17:39:00 +0700

    add search page

--- src/main/java/controller/home/HomeServlet.java
+++ src/main/java/controller/home/HomeServlet.java
@@ -1,5 +1,6 @@
 package controller.home;
 
+import model.Food;
 import service.deal.DealService;
 import service.deal.IDealService;
 import service.food.FoodService;
@@ -15,6 +16,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 import static java.lang.Integer.parseInt;
 
@@ -23,9 +25,9 @@ public class HomeServlet extends HttpServlet {
     private IDealService dealService = new DealService();
     private ITagService tagService = new TagService();
     private IShopService shopService = new ShopService();
-    
+
     private IFoodService foodService = new FoodService();
-    
+
     private IServiceService serviceService = new ServiceService();
 
     @Override
@@ -43,6 +45,12 @@ public class HomeServlet extends HttpServlet {
                 case "showByTag":
                     showByTag(request, response);
                     break;
+                case "listItem":
+                    listItem(request, response);
+                    break;
+                case "find":
+                    showFind(request, response);
+                    break;
                 default:
                     showHome(request, response);
                     break;
@@ -54,29 +62,43 @@ public class HomeServlet extends HttpServlet {
         }
     }
 
+    private void showFind(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("items", foodService.fillAll());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("client/assets/page/find.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void listItem(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String searchFood = request.getParameter("searchFood");
+        List<Food> foods = foodService.selectFoodByName(searchFood);
+        request.setAttribute("foods", foods);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("client/assets/page/find.jsp");
+        dispatcher.forward(request, response);
+    }
+
     private void showAllTag(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("client/assets/page/client/listFoodSearchByTag.jsp");
-        request.setAttribute("foods",foodService.fillAll());
-        dispatcher.forward(request,response);
+        request.setAttribute("foods", foodService.fillAll());
+        dispatcher.forward(request, response);
     }
 
     private void showByTag(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("client/assets/page/client/listFoodSearchByTag.jsp");
         int id = Integer.parseInt(request.getParameter("id"));
-        request.setAttribute("foods",foodService.findAllFoodByIdTag(id));
-        dispatcher.forward(request,response);
+        request.setAttribute("foods", foodService.findAllFoodByIdTag(id));
+        dispatcher.forward(request, response);
     }
 
     private void showHome(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("client/assets/page/home.jsp");
 
-        request.setAttribute("deals",dealService.fillAll());
-        request.setAttribute("tags",tagService.fillAll());
-        request.setAttribute("shops",shopService.fillAll());
-        request.setAttribute("foods",foodService.fillAll());
-        request.setAttribute("services",serviceService.fillAll());
-        dispatcher.forward(request,response);
+        request.setAttribute("deals", dealService.fillAll());
+        request.setAttribute("tags", tagService.fillAll());
+        request.setAttribute("shops", shopService.fillAll());
+        request.setAttribute("foods", foodService.fillAll());
+        request.setAttribute("services", serviceService.fillAll());
+        dispatcher.forward(request, response);
     }
 
     @Override
@@ -87,10 +109,13 @@ public class HomeServlet extends HttpServlet {
         }
         switch (action) {
             case "showByTag":
-                   showByTag(request, response);
+                showByTag(request, response);
+                break;
+            case "find":
+                showFind(request, response);
                 break;
             default:
-                    showHome(request, response);
+                showHome(request, response);
                 break;
         }
     }
